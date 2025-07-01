a79c9c09805a6e35c81e607de27adb993ad7fdf7
yalex-kim <yunbae.alex.kim@gmail.com>
2025-07-01 15:11:57 +0900

    add getSumSum()

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,12 +4,24 @@
 class Cal {
 public:
     // 코드 작성
+
+
+    int getSumSum(int a, int b, int c) {
+        return a + b + c;
+	}
 };
 
 TEST(TS, tc1)
 {
     EXPECT_EQ(1, 1);
 }
+
+TEST(TS, tc_getSumSum)
+{
+    Cal cal;
+    EXPECT_EQ(cal.getSumSum(1, 2, 3), 6);
+}
+
 int main()
 {
     ::testing::InitGoogleMock();
