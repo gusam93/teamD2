004d6b9a05e57a657f5a4d810ee67fe5c3817786
sk.lee <moonstar11430@gmail.com>
2025-07-01 15:10:41 +0900

    zegop추가

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,12 +4,25 @@
 class Cal {
 public:
     // 코드 작성
+    int getZegop(int a)
+    {
+        return a * a;
+    }
 };
 
 TEST(TS, tc1)
 {
     EXPECT_EQ(1, 1);
 }
+
+TEST(TS, tc_zegop) {
+    Cal cal;
+
+    int actual = cal.getZegop(3);
+    int expect = 9;
+    EXPECT_EQ(expect, actual);
+}
+
 int main()
 {
     ::testing::InitGoogleMock();
